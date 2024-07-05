b8e25f27e26d90d916a8c3253e182d3c3dfea00f
RajRohitYadav9 <rjrohityadav19@gmail.com>
2024-07-05 13:44:30 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_session import Session
+from flask_cors import CORS
+from datetime import timedelta
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "https://agreeable-ground-084082a1e.5.azurestaticapps.net"}})
+app.config['SECRET_KEY'] = 'my_secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+sess = Session(app)
+
+
+@app.route('/', methods=['GET'])
+def Home():
+    return "Hello world"
+
+from api import *
+
+if __name__ == '__main__':
+    app.run(debug=True)
