4d75111479c66b86ee290bc3b46135950583900f
RajRohitYadav9 <rjrohityadav19@gmail.com>
2024-07-05 18:26:10 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_session import Session
+# from flask_session import Session
 from flask_cors import CORS
 from datetime import timedelta
 
